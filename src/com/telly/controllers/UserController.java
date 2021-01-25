f652d911d5a25ed00764f8160f054a7eeb1968f2
IhebChaker <hi_benakcha@esi.dz>
2021-01-25 14:00:24 +0100

    modified UserController

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.telly.dao.FormValidationGroup;
-import com.telly.dao.Reserve;
 import com.telly.dao.User;
-import com.telly.service.ReserveService;
 import com.telly.service.UserService;
 
 
@@ -24,19 +22,6 @@ public class UserController {
 
 	@Autowired
 	UserService userService;
-	
-	@Autowired
-	ReserveService reserveService;
-
-	@RequestMapping("/login")
-	public String showLogin() {
-		return "login";
-	}
-	
-	@RequestMapping("/loggedout")
-	public String showLogout() {
-		return "loggedout";
-	}
 	
 	
 	@RequestMapping("/createaccount")
@@ -63,38 +48,6 @@ public class UserController {
 
 	}
 
-	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
-	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
-		
-		if (result.hasErrors()) {
-			return "reservebus";
-		}
-		
-		String username = principal.getName();
-		reserve.getUser().setUsername(username);
-		
-		reserveService.reserve(reserve);
-	
-		
-		return "home";
-
-	}
-	
-	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
-	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
-		
-		
-		String username = principal.getName();
-		reserve.getUser().setUsername(username);
-		
-		List<Reserve> reserves = reserveService.getReserves(username);
-		model.addAttribute("reserves", reserves);
-		System.out.println(reserves);
-	
-		
-		return "home";
-
-	}
 	
 
 }
